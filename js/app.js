b31f21c7bf6ef9a9b20be044962eccc806761eff
Kanav Khurana <kanavkhurana@gmail.com>
2015-01-10 12:34:59 +0530

    Final version

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function fizzBuzz(number){
+	if(number % 15 == 0){
+		return 'fizzbuzz';
+	}
+	else if(number % 3 == 0){
+		return 'fizz';
+	}
+	else if(number % 5 ==0)
+	{
+		return 'buzz';
+	}
+	else{
+		return number;
+	}
+}
+
+$(document).ready(function(){
+var start=1;
+var end=100;
+//alert('hello');
+for(var i = start ; i <= end; i++ ){
+
+$('body').append(fizzBuzz(i) + ' ');
+
+}
+
+})
+
