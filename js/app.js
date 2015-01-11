0fde8fd3b0c71a6bbf9f2ecb9e38bbda9fb48139
Kanav Khurana <kanavkhurana@gmail.com>
2015-01-11 15:36:33 +0530

    Bettered formatting, prompt

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
-var START = 1;
-var END = 100;
-
-function fizzBuzz(number){
-	if(number % 15 == 0){
-		return 'fizzbuzz';
-	}
-	else if(number % 3 == 0){
-		return 'fizz';
-	}
-	else if(number % 5 ==0)
-	{
-		return 'buzz';
-	}
-	else{
-		return number;
-	}
+function fizzBuzz(number) {
+    if (number % 15 === 0) {
+        return 'fizzbuzz';
+    } else if (number % 3 === 0) {
+        return 'fizz';
+    } else if (number % 5 === 0) {
+        return 'buzz';
+    } else {
+        return number;
+    }
 }
 
-$(document).ready(function(){
-//var START = 1;
-//var END = 100;
-//alert('hello');
-for(var i = START ; i <= END; i++ ){
+$(document).ready(function() {
+        
+        var start = 1;
 
-$('body').append(fizzBuzz(i) + ' ');
+        var end = prompt("Please enter the number up to which the fizzbuzz sequence needs to generated", 100);
+        if (!end) {
+            alert('Please enter a number!');
+        } else if (isNaN(end)) {
+            alert('Please enter a valid number!');
+        } else if(end % 1 != 0){
+        	alert('Please enter a whole number!');
+        } else{
 
-}
+            for (var i = start; i <= end; i++) {
 
-})
+                $('body').append('<p class="number">'+ i + ': ' + fizzBuzz(i) + '</p>');
 
+            }
+        }
+
+    })
+    /*
+    Ask the user to input a number - use prompt() - DONE
+    Make the output more readible 
+    Use brackets.io for jslint - can try
+    */
